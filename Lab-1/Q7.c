a66b0a4001af2b8eb2b43a025f4ffef4199c1f64
Ashrith Sagar <ashrith9sagar@gmail.com>
2022-12-23 10:17:11 +0530

    L1Q7 1

---
+++ Lab-1/Q7.c
@@ -0,0 +1,21 @@
+// Preprocessor directives.
+#include <stdio.h>
+
+int main() // Start main.
+{ // Start.
+
+	// Variable initialisations.
+	float C, F;
+	
+	// Ask user for input.
+	printf("Enter temperature in Fahrenheit: \n");
+	scanf("%f", &F);
+
+	C = (5.0/9) * (F - 32);
+
+	// Display output.
+	printf("The given temperature in Centigrade is %f C.\n", C);
+
+	printf("\nMy name is Ashrith Sagar Yedlapalli.\n");
+
+} // End.
